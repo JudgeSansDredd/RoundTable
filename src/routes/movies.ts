819d9e87db9b80b7e69e18a831a810321aa823a2
Nathan Stanley <nstanley@bandwidth.com>
2024-10-04 11:13:28 -0400

    incomplete: Remove some lines and bork things

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,75 +1,46 @@
 import express, { Request, Response } from "express";
-import { z } from "zod";
 import PrismaClientSingleton from "../../prisma/PrismaClientSingleton";
-import { MovieSchema, QueryParamSchema } from "../utils/validation";
 
 const router = express.Router();
 const db = PrismaClientSingleton.getInstance();
 
 // All routes in this file will be prefixed with /movies
 router.get("/", async (req: Request, res: Response) => {
-  try {
-    const params = QueryParamSchema.parse(req.query);
-    const movies = await db.movie.findMany({
-      where: {
-        runTime: {
-          gte: params.runTimeMin,
-          lte: params.runTimeMax,
-        },
-        releaseDate: {
-          lte: params.released ? new Date() : undefined,
-          gte: params.releasedAfter?.toJSDate(),
-        },
-      },
-      include: {
-        actors: params.actors,
-      },
-    });
-    res.json(movies);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+  const movies = await db.movie.findMany();
+
+  res.json(movies);
 });
 
 router.get("/:id", async (req: Request, res: Response) => {
-  try {
-    const { id } = z.object({ id: z.string().uuid() }).parse(req.params);
-    const movie = await db.movie.findUnique({
-      where: { id },
-      include: { actors: true },
-    });
+  const { id } = req.params;
+  const movie = await db.movie.findUnique({
+    where: { id },
+    include: { actors: true },
+  });
 
-    if (!movie) {
-      res.status(404).json({ message: "Movie not found" });
-      return;
-    }
-
-    res.json(movie);
-  } catch (error) {
-    res.status(500).json(error);
+  if (!movie) {
+    res.status(404).json({ message: "Movie not found" });
+    return;
   }
+
+  res.json(movie);
 });
 
 router.get("/:id/genres", async (req: Request, res: Response) => {
   const { id } = req.params;
-  try {
-    const movie = await db.movie.findUnique({
-      where: { id },
-      include: { actors: true },
-    });
-    if (!movie) {
-      res.status(404).json({ message: "Movie not found" });
-      return;
-    }
-    const movieValidated = MovieSchema.parse(movie);
+  const movie = await db.movie.findUnique({
+    where: { id },
+    include: { actors: true },
+  });
 
-    const genres = movieValidated.genres.map((genre) => genre.toLowerCase());
-
-    res.json(genres);
+  if (!movie) {
+    res.status(404).json({ message: "Movie not found" });
     return;
-  } catch (error) {
-    res.status(500).json(error);
   }
+
+  const genres = movie.genres.map((genre) => genre.toLowerCase());
+
+  res.json(genres);
 });
 
 export default router;
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,32 +1,9 @@
-import { DateTime } from "luxon";
 import { z } from "zod";
 
-export const QueryParamSchema = z
-  .object({
-    runTimeMax: z.coerce.number(),
-    runTimeMin: z.coerce.number().min(0),
-    released: z.enum(["true", "false"]).transform((val) => val === "true"),
-    releasedAfter: z
-      .string()
-      .datetime()
-      .transform((val) => DateTime.fromISO(val)),
-    actors: z.enum(["true", "false"]).transform((val) => val === "true"),
-  })
-  .partial();
+export const QueryParamSchema = z.object({});
 
-export const ActorSchema = z.object({
-  id: z.string().uuid(),
-  name: z.string(),
-});
+export const ActorSchema = z.object({});
 
-export const MovieSchema = z.object({
-  id: z.string().uuid(),
-  name: z.string(),
-  genres: z.array(z.string()),
-  runTime: z.number(),
-  releaseDate: z.date().transform((val) => DateTime.fromJSDate(val)),
-});
+export const MovieSchema = z.object({});
 
-export const MovieWithActorSchema = MovieSchema.extend({
-  actors: z.array(ActorSchema),
-});
+export const MovieWithActorSchema = MovieSchema.extend({});
